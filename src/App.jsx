7d9004df737ee8e1602b23a7a4dc7315055bd416
CoreySumma <CoreySumma@gmail.com>
2023-01-24 17:42:49 -0600

    successfully consumed AI API to create an image based on description

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 
 export default function App() {
   const [prompt, setPrompt] = useState('');
+  const [result, setResult] = useState('');
   const configuration = new Configuration({
     apiKey: import.meta.env.VITE_Open_AI_Key,
   });
@@ -16,20 +17,21 @@ export default function App() {
       n: 1,
       size: "1024x1024",
   });
-  console.log(res.data.data[0].url);
+    setResult(res.data.data[0].url);  
 }
 
   return (
-    <>
     <div className="app-main">
-   <h2>Let's Do This</h2>
+   <h2>Image Generator</h2>
+    <p>Please provide a description</p>
    <input 
     className='app-input' 
-    placeholder='Type A Description'
+    placeholder='Describe The Image You Want'
     onChange={(e) => setPrompt(e.target.value)}
     />
-   <button onClick={generateImage}>Generate Image</button>
+   <button onClick={generateImage}>Generate</button>
+   
+   {result.length > 0 ? <img className='image-result' src={result} alt="result" /> : <></>}
     </div>
-  </>
   )
 }
